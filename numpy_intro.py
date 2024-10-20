dbf22e7805abcd923c1809ce60e515c60d4d435a
AlekseySapi <altmind92@gmail.com>
2024-10-20 19:44:47 +0300

    Initial commit

---
+++ numpy_intro.py
@@ -0,0 +1,11 @@
+# Изучаю NumPy
+
+import numpy as np
+
+# Создание массива
+array = np.array([1, 2, 3, 4, 5])
+
+# Операции с массивами
+print("Массив:", array)
+print("Сумма элементов:", np.sum(array))
+print("Среднее значение:", np.mean(array))
