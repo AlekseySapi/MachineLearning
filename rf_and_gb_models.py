12ad6e7b30cfc4498ee052e2b9576b766d3ed999
AlekseySapi <altmind92@gmail.com>
2024-11-01 23:36:34 +0300

    Подключаем библиотеки

--- rf_and_gb_models.py
+++ rf_and_gb_models.py
@@ -1,3 +1,10 @@
 # Сравним RF и GB модели
 
 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error
+
+
