d83c5f59aa31b6d17d41581e03ebac5e33226308
AlekseySapi <altmind92@gmail.com>
2024-11-01 23:40:08 +0300

    Добавляем признаки

--- rf_and_gb_models.py
+++ rf_and_gb_models.py
@@ -8,3 +8,12 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_absolute_error
 
 
+# Генерация данных
+np.random.seed(42)
+data_size = 500
+
+# Признаки: день недели, час и температура
+day_of_week = np.random.randint(1, 8, data_size)
+hour = np.random.randint(0, 24, data_size)
+temperature = np.random.normal(15, 10, data_size)  # Средняя температура около 15°C
+
