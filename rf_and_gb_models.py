51a5f229b287b5e27c8cfb2ab5f931e7d0bcb071
AlekseySapi <altmind92@gmail.com>
2024-11-02 22:38:57 +0300

    Разделим на обучающую и тестовую выборки

--- rf_and_gb_models.py
+++ rf_and_gb_models.py
@@ -31,3 +31,8 @@ df = pd.DataFrame({
     'temperature': temperature,
     'consumption': consumption
 })
+
+# Разделим на обучающую и тестовую выборки
+X = df[['day_of_week', 'hour', 'temperature']]
+y = df['consumption']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
